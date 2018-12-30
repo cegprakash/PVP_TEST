83936249f5c50470cffaa8414198224ba87e5046
cegprakash <cegprakash@gmail.com>
2018-12-30 15:04:24 +0530

    added design for user app

--- app/src/main/java/com/a1education/cegprakash/a1educationalcenter/LoginTestActivity.java
+++ app/src/main/java/com/a1education/cegprakash/a1educationalcenter/LoginTestActivity.java
@@ -4,28 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 
-public class StartMenuActivity extends AppCompatActivity {
+public class LoginTestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_menu);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        final Button button = findViewById(R.id.take_test_button);
+        setContentView(R.layout.activity_login_test);
+        final Button button = findViewById(R.id.login_button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent switchActivityIntent = new Intent(StartMenuActivity.this, LoginTestActivity.class);
+                Intent switchActivityIntent = new Intent(LoginTestActivity.this, MainActivity.class);
                 startActivity(switchActivityIntent);
             }
         });
-
-
     }
 }
